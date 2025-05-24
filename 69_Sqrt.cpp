1aca906ea8f839a8dfde597f3be47c1b688160c3
Sakibul Hasan Rafi <iamshrafi@gmail.com>
2025-05-24 22:23:35 +0600

    69_Sqrt

---
+++ 69_Sqrt.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long temp = x;
+        while(temp * temp > x){
+            temp = (temp + x/temp) / 2;
+        }
+        return temp;
+    }
+};
