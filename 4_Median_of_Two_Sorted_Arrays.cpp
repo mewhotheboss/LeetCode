a65b5b245ffd0fd0f28851d67423ac0d6dcf6d59
Sakibul Hasan Rafi <iamshrafi@gmail.com>
2025-10-02 21:47:37 +0600

    -

---
+++ 4_Median_of_Two_Sorted_Arrays.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> merged;
+        merged.reserve(nums1.size() + nums2.size());
+        merged.insert(merged.end(), nums1.begin(), nums1.end());
+        merged.insert(merged.end(), nums2.begin(), nums2.end());
+
+        sort(merged.begin(), merged.end());
+
+        int n = merged.size();
+        
+        if (n % 2 != 0) {
+            return merged[n / 2];
+        } else {
+            return (merged[n / 2 - 1] + merged[n / 2]) / 2.0;
+        }
+    }
+};
---
+++ 7_Reverse_Integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long result = 0;
+
+        while(x != 0)
+        {
+            int digit = x % 10;
+            x = x/10;
+            result = result*10 + digit;
+        }
+
+        if(result < INT_MIN || result > INT_MAX)
+        {
+            return 0;
+        }
+
+        return (int)result;
+    }
+};
